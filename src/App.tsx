b36c4992365a0c4a99eda677c3a718ddafc745d0
daviddprtma <davidchristianpratama@gmail.com>
2024-09-27 12:08:07 +0700

    learning advance state management with redux done

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import { useAppDispatch, useAppSelector } from "./app/hooks";
+import { increment } from "./app/store";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const count = useAppSelector((state) => state.value);
+  const dispatch = useAppDispatch();
 
   return (
     <>
@@ -18,7 +20,7 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <button type="button" onClick={() => setCount((count) => count + 1)}>
+        <button type="button" onClick={() => dispatch(increment(1))}>
           count is {count}
         </button>
         <p>
@@ -29,7 +31,7 @@ function App() {
         Click on the Vite and React logos to learn more
       </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
